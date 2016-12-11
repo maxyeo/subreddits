25967b16914e7e3208957478dea9dcab9e8562fa
Michael Ashmead <ashmeadmichael@gmail.com>
2016-12-10 20:11:45 -0500

    Updated lambda means algorithm.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Datasets reside in a folder called data which is gitignored because they are too
 
 ### Lambda Means on the Complete Data Set
 To train lambda_means algorithm:
-`python classify.py --mode train --data output/subreddit_activity_train.txt --model-file output/subreddit_activity_model.txt --algorithm lambda_means`
+`python classify.py --mode train --data subreddits --model-file output/_activity_model.txt --algorithm lambda_means`
 
 To run lambda_means algorithm:
 `python classify.py --mode test --algorithm lambda_means --model-file output/subreddit_activity_model.txt --data output/subreddit_activity_test.txt --predictions-file output/subreddit_activity_predictions.txt`
--- lambda_means.py
+++ lambda_means.py
@@ -1,6 +1,5 @@
 from cs475_types import Predictor
 import math
-import time
 
 # LambdaMeans is a subclass of Predictor
 class LambdaMeans(Predictor):
@@ -8,15 +7,9 @@ class LambdaMeans(Predictor):
         self.clustering_training_iterations = clustering_training_iterations
         self.subreddits = subreddits
         # Find mean of all instances and initialize the prototype vector to the mean of all instances
+
         # the size of prototype should be the maximum feature index
-        self.maximum = 0
-        for instance in instances:
-            instanceMax = 0
-            for key in instance._feature_vector.getFeatureVector().iterkeys():
-                if key > instanceMax:
-                    instanceMax = key
-            if instanceMax > self.maximum:
-                self.maximum = instanceMax
+        self.maximum = self.find_max_index(instances)
 
         # initialize prototype with 0s
         prototype = [0] * (self.maximum)
@@ -24,14 +17,14 @@ class LambdaMeans(Predictor):
         for x in range(1, self.maximum + 1):
             for instance in instances:
                 value = instance._feature_vector.getFeatureVector().get(x)
-                if (value is not None):
+                if value is not None:
                     prototype[x - 1] += value
             prototype[x - 1] /= len(instances)
 
         self.clambda = cluster_lambda
         # Set the value of lambda to the average distance from each training instance to the mean prototype vector
         distances = 0
-        if (self.clambda == 0.0):
+        if self.clambda == 0.0:
             for instance in instances:
                 running_square_sum = 0
                 for x in range(1, self.maximum + 1):
@@ -41,99 +34,57 @@ class LambdaMeans(Predictor):
                     prototypeVal = prototype[x - 1]
 
                     running_square_sum += math.pow(instanceVal - prototypeVal, 2)
-                distances += math.sqrt(running_square_sum)
+                distances += running_square_sum
 
             # Find average of all instances
             self.clambda = distances / len(instances)
 
         self.num_clusters = 1
-        self.prototypes = []
-        self.prototypes.append(prototype)
+        self.prototypes = [prototype]
 
     def train(self, instances):
-        prototypeClusters = [[]]
-        for x in range(1, self.clustering_training_iterations):
-            counter = 0
+        for x in range(1, self.clustering_training_iterations + 1):
+            print x
+            prototypeClusters = [] # A list of lists
+            for prototype in self.prototypes:
+                prototypeClusters.append([])
             for instance in instances:
-                counter += 1
-                if counter % 100 == 0:
-                    print(counter)
                 # check which prototype the instance belongs to
-                closestPrototypeIndex = -1
-                closestPrototypeDistance = float("inf")
-                curIndex = 0
-                for prototype in self.prototypes:
-                    running_square_sum = 0
-                    for x in range(1, self.maximum + 1):
-                        instanceVal = instance._feature_vector.getFeatureVector().get(x)
-                        if instanceVal is None:
-                            instanceVal = 0
-                        prototypeVal = prototype[x - 1]
-
-                        running_square_sum += math.pow(instanceVal - prototypeVal, 2)
-                    distance = math.sqrt(running_square_sum)
-                    if (distance < closestPrototypeDistance):
-                        closestPrototypeDistance = distance
-                        closestPrototypeIndex = curIndex
-                    curIndex += 1
+                closestPrototype = self.getClosestPrototypeIndexAndDistance(instance)
+                closestPrototypeIndex = closestPrototype[0]
+                closestPrototypeDistance = closestPrototype[1]
 
                 # if instance is farther than lambda
-                if (closestPrototypeDistance > self.clambda):
+                if closestPrototypeDistance > self.clambda:
                     # create a new prototype
-                    # initialize prototype with 0s
-                    prototype = [0] * (self.maximum)
-
-                    for x in range(1, self.maximum + 1):
-                        value = instance._feature_vector.getFeatureVector().get(x)
-                        if (value is not None):
-                            prototype[x - 1] = value
+                    prototype = self.create_prototype(instance._feature_vector.getFeatureVector())
                     self.prototypes.append(prototype)
 
-                    # assign instance to cluster
-                    prototypeClusters.append([])
-                    prototypeClusters[-1].append(instance)
+                    # assign instance to new prototype
+                    prototypeClusters.append([instance])
                 else:
                     prototypeClusters[closestPrototypeIndex].append(instance)
 
+            print x
             # maximization step
             curCluster = 0
-            counter = 0
             for cluster in prototypeClusters:
-                counter += 1
-                print counter
                 # if the cluster is empty make the prototype 0
                 newprototype = [0] * (self.maximum)
-                if (len(cluster) > 0):
+                if len(cluster) > 0:
                     for x in range(1, self.maximum + 1):
                         for instance in cluster:
                             value = instance._feature_vector.getFeatureVector().get(x)
-                            if (value is not None):
+                            if value is not None:
                                 newprototype[x - 1] += value
                         newprototype[x - 1] /= len(cluster)
 
                 self.prototypes[curCluster] = newprototype
                 curCluster += 1
+            print x
 
     def predict(self, instance):
-        closestPrototypeIndex = -1
-        closestPrototypeDistance = float("inf")
-        curIndex = 0
-        for prototype in self.prototypes:
-            running_square_sum = 0
-            for x in range(1, self.maximum + 1):
-                instanceVal = instance._feature_vector.getFeatureVector().get(x)
-                if instanceVal is None:
-                    instanceVal = 0
-                prototypeVal = prototype[x - 1]
-
-                running_square_sum += math.pow(instanceVal - prototypeVal, 2)
-            distance = math.sqrt(running_square_sum)
-            if (distance < closestPrototypeDistance):
-                closestPrototypeDistance = distance
-                closestPrototypeIndex = curIndex
-            curIndex += 1
-
-        # The closest prototype is stored in closestPrototypeIndex
+        closestPrototypeIndex = self.getClosestPrototypeIndexAndDistance(instance)[0]
         closestPrototype = self.prototypes[closestPrototypeIndex]
         highestFeatureValue = closestPrototype[0]
         highestFeatureIndex = 0
@@ -148,3 +99,52 @@ class LambdaMeans(Predictor):
         if int(str(instance._label)) % 100 == 0:
             print str(instance._label)
         return self.subreddits[highestFeatureIndex]
+
+    def find_max_index(self, instances):
+        maximum = -1
+        for instance in instances:
+            instanceMax = 0
+            for key in instance._feature_vector.getFeatureVector().iterkeys():
+                if key > instanceMax:
+                    instanceMax = key
+            if instanceMax > maximum:
+                maximum = instanceMax
+
+        return maximum
+
+    def getClosestPrototypeIndexAndDistance(self, instance):
+        closestPrototypeIndex = -1
+        closestPrototypeDistance = float("inf")
+        curIndex = 0
+        for prototype in self.prototypes:
+            distance = self.get_distance(prototype, instance._feature_vector.getFeatureVector())
+
+            if (distance < closestPrototypeDistance):
+                closestPrototypeDistance = distance
+                closestPrototypeIndex = curIndex
+            curIndex += 1
+
+        return (closestPrototypeIndex, closestPrototypeDistance)
+
+    def create_prototype(self, instance_feature_vector):
+        # initialize prototype with 0s
+        prototype = [0] * self.maximum
+
+        for x in range(1, self.maximum + 1):
+            value = instance_feature_vector.get(x)
+            if value is not None:
+                prototype[x - 1] = value
+
+        return prototype
+
+    def get_distance(self, prototype, instance_feature_vector):
+        running_square_sum = 0
+        for x in range(0, self.maximum):
+            instanceVal = instance_feature_vector.get(x + 1)
+            if instanceVal is None:
+                instanceVal = 0
+            prototypeVal = prototype[x]
+
+            running_square_sum += math.pow(instanceVal - prototypeVal, 2)
+
+        return running_square_sum
